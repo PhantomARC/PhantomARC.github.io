4b45cc72cf9416393e5be7e4c195650c866eac58
ARC <51982416+PhantomARC@users.noreply.github.com>
2024-11-29 20:06:36 -0800

    Update default.js

--- res/html/default.js
+++ res/html/default.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
 	console.log("YEP");
-	fetch('https://PhantomARC.github.io/res/html/header.html')
+	fetch('https://github.com/PhantomARC/PhantomARC.github.io/blob/main/res/html/header.html')
 		.then(response => response.text())
 		.then(data => document.querySelector('header').innerHTML = data);
 
-	fetch('https://PhantomARC.github.io/res/html/footer.html')
+	fetch('https://github.com/PhantomARC/PhantomARC.github.io/blob/main/res/html/footer.html')
 		.then(response => response.text())
 		.then(data => document.querySelector('footer').innerHTML = data);
 });
